465fff09a73c53876a6aca2c54595c2a87388621
akash619 <akash619@protonmail.com>
2022-01-14 15:43:44 +0530

    update unpkg module

--- src/plugins/unpkg-path-plugins.ts
+++ src/plugins/unpkg-path-plugins.ts
@@ -1,4 +1,16 @@
+import axios from "axios";
 import * as esbuild from "esbuild-wasm";
+import localforage from "localforage";
+
+const filecache = localforage.createInstance({
+  name: "filecache",
+});
+
+(async () => {
+  await filecache.setItem("color", "red");
+  const color = await filecache.getItem("color");
+  console.log(color);
+})();
 
 export const unpkgPathPlugin = () => {
   return {
@@ -6,7 +18,31 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+
+        // else if (args.path === "tiny-test-pkg") {
+        //   return {
+        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+        //     namespace: "a",
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +52,33 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('react-dom')
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cachedResult = await filecache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        // if it is in the cache, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+        // store response in cache
+        await filecache.setItem(args.path, result);
+        return result;
       });
     },
   };
