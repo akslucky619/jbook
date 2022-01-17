9c71a3b18f9a09f9f60b08fc37e257f209558ced
akash619 <akash619@protonmail.com>
2022-01-18 02:22:58 +0530

    add markdown css

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,40 @@
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
-
+import "./text-editor.css";
 const TextEditor = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div className="text-editor" onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
     </div>
   );
 };
