21b2e4b2f544938196d51adf92f5ccc7950a4210
akash619 <akash619@protonmail.com>
2022-01-19 14:55:54 +0530

    update

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -4,6 +4,7 @@ import "./text-editor.css";
 const TextEditor = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -27,14 +28,16 @@ const TextEditor = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
-    <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
